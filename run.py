a6768d937512c8455536f2a390f8980f3be391d1
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2017-05-23 08:42:52 -0300

    Code style

--- run.py
+++ run.py
@@ -1,30 +1,31 @@
-from flask import Flask, request, jsonify
-from gevent.wsgi import WSGIServer
 import pprint
+
+from flask import Flask, jsonify, request
+from gevent.wsgi import WSGIServer
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 ALL_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 
+
 @app.route('/', defaults={'path': ''}, methods=ALL_METHODS)
 @app.route('/<path:path>', methods=ALL_METHODS)
 def index(path):
     data = dict(
-        path = request.path,
-        method = request.method,
-        headers = list(request.headers.items()),
-        form = list(request.form.items()),
-        args = list(request.args.items()),
-        files = [(f[0], f[1].filename) for f in request.files.items()]
-        json = request.json,
-        content_type = request.content_type,
-    )
+        path=request.path,
+        method=request.method,
+        headers=list(request.headers.items()),
+        form=list(request.form.items()),
+        args=list(request.args.items()),
+        files=[(f[0], f[1].filename) for f in request.files.items()],
+        json=request.json,
+        content_type=request.content_type, )
     app.logger.info('\n' + pprint.pformat(data))
     return jsonify(data)
+
 
 if __name__ == '__main__':
     http_server = WSGIServer(('', 5000), app)
     app.logger.info('RUNNING')
     http_server.serve_forever()
-
