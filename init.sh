8c1381c7005b938a9568588dfe2ac15a46831853
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2019-07-04 20:56:12 -0300

    fix: correct all shellcheck warnings

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ PORT_IPV4=${PORT_IPV4:-$PORT}
 PORT_IPV6=${PORT_IPV6:-5001}
 
 exec waitress-serve \
-  --listen=0.0.0.0:${PORT_IPV4} \
-  --listen=[::]:${PORT_IPV6} \
+  --listen="0.0.0.0:${PORT_IPV4}" \
+  --listen="[::]:${PORT_IPV6} "\
   run:app
