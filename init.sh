f20c16088c15b276a79c1a5c78832be5c90be0cf
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2019-07-04 21:08:03 -0300

    refactor: remove PORT_IPV4 and 6

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 PORT=${PORT:-5000}
-PORT_IPV4=${PORT_IPV4:-$PORT}
-PORT_IPV6=${PORT_IPV6:-5001}
 
+echo "STARTING"
 exec waitress-serve \
-  --listen="0.0.0.0:${PORT_IPV4}" \
-  --listen="[::]:${PORT_IPV6} "\
+  --host="0.0.0.0" \
+  --port="${PORT}" \
   run:app
